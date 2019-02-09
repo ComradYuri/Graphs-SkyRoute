812b70b0113fc96790221b8cbf800334b30680ce
Joerie <joerie_svd@hotmail.com>
2019-02-09 19:44:42 +0100

    Assignment up until step 44

--- skyroute.py
+++ skyroute.py
@@ -7,6 +7,8 @@ landmark_string = ""
 for key in landmark_choices.keys():
     landmark_string += (key + " - " + landmark_choices[key] + '\n')
 
+stations_under_construction = ['Moody Centre']
+
 
 def greet():
     print("Hi there and welcome to SkyRoute!")
@@ -62,8 +64,12 @@ def get_end():
 def new_route(start_point=None, end_point=None):
     start_point, end_point = set_start_and_end(start_point, end_point)
     shortest_route = get_route(start_point, end_point)
-    shortest_route_string = '\n'.join(shortest_route)
-    print("The shortest metro route from {} to {} is:\n{}".format(start_point, end_point, shortest_route_string))
+    if shortest_route:
+        shortest_route_string = '\n'.join(shortest_route)
+        print("The shortest metro route from {} to {} is:\n{}".format(start_point, end_point, shortest_route_string))
+    else:
+        print("Unfortunately, there is currently no path between {0} and {1} due to maintenance.".format(start_point,
+                                                                                                         end_point))
     again = input("Would you like to see another route? Enter y/n: ")
     if again == 'y':
         show_landmarks()
@@ -76,6 +82,7 @@ def get_route(start_point, end_point):
     routes = []
     for start_station in start_stations:
         for end_station in end_stations:
+            metro_system = get_active_stations() if stations_under_construction else vc_metro
             route = bfs(vc_metro, start_station, end_station)
             if route:
                 routes.append(route)
@@ -89,8 +96,20 @@ def show_landmarks():
         print(landmark_string)
 
 
+def get_active_stations():
+    updated_metro = vc_metro
+    for station_under_construction in stations_under_construction:
+        for current_station, neighboring_stations in vc_metro.items():
+            if current_station != station_under_construction:
+                updated_metro[current_station] -= set(stations_under_construction)
+            else:
+                updated_metro[current_station] = set([])
+    return updated_metro
+
+
 def goodbye():
     print("Thanks for using SkyRoute!")
 
 
-skyroute()
+#skyroute()
+print(get_active_stations())
